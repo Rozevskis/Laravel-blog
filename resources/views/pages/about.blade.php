5a421d8ef6597476ddd77344826fa6e916420a5c
Kristofers Jēkabs Rozevskis <44755996+Rozevskis@users.noreply.github.com>
2024-01-05 17:23:11 +0200

    Updated with Blade

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,103 +1,18 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>About me</title>
+@extends('main')
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  
-
-</head>
-  <body class="bg-light">
-    <!-- Default bootstrap navbar -->
-   <nav
-    class="navbar navbar-expand-sm navbar-light bg-light"
-   >
-    <div class="container">
-        <a class="navbar-brand" href="/">Laravel blog</a>
-        <button
-            class="navbar-toggler d-lg-none"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapsibleNavId"
-            aria-controls="collapsibleNavId"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-        >
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="collapsibleNavId">
-            <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="/">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="/about" aria-current="page"
-                        >About
-                        <span class="visually-hidden">(current)</span></a
-                    >
-                </li>
-                
-                <li class="nav-item">
-                    <a class="nav-link" href="/contact">Contact</a>
-                </li>
-                
-            </ul>
-            
-                    <a
-                        class="nav-link dropdown-toggle"
-                        href="#"
-                        id="dropdownId"
-                        data-bs-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false"
-                        >My account</a
-                    >
-                    <div
-                        class="dropdown-menu"
-                        aria-labelledby="dropdownId"
-                    >
-                        <a class="dropdown-item" href="#"
-                            >Action 1</a
-                        >
-                        <a class="dropdown-item" href="#"
-                            >Action 2</a
-                        >
-                    </div>
-                
-        </div>
-    </div>
-   </nav>
+@section('content')
    
    <div class="p-5 mb-4 bg-light rounded-3">
-    <div class="container-fluid py-5">
-        <h1 class="display-5 fw-bold">About me</h1>
-        <p class="col-md-8 fs-4">
-           Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-           Soluta, recusandae ab, perferendis consequuntur libero nam 
-           iusto, suscipit cum fuga voluptate error quaerat adipisci aut 
-           nihil voluptas similique nulla facilis natus?
-        </p>
-        
-    </div>
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">About me</h1>
+            <p class="col-md-8 fs-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Soluta, recusandae ab, perferendis consequuntur libero nam 
+            iusto, suscipit cum fuga voluptate error quaerat adipisci aut 
+            nihil voluptas similique nulla facilis natus?
+            </p>
+            
+        </div>
    </div>
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  </body>
-</html>
+        @endsection
+    
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,76 +1,6 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel Blog</title>
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  
+@extends('main')
 
-</head>
-  <body class=" bg-light">
-    <!-- Default bootstrap navbar -->
-   <nav
-    class="navbar navbar-expand-sm navbar-light bg-light "
-   >
-    <div class="container">
-        <a class="navbar-brand" href="/">Laravel blog</a>
-        <button
-            class="navbar-toggler d-lg-none "
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapsibleNavId"
-            aria-controls="collapsibleNavId"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-        >
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="collapsibleNavId">
-            <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-
-                <li class="nav-item">
-                    <a class="nav-link active " href="#" aria-current="page"
-                        >Home
-                        <span class="visually-hidden">(current)</span></a
-                    >
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link "  href="/about">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/contact">Contact</a>
-                </li>
-                
-            </ul>
-            
-                    <a
-                        class="nav-link dropdown-toggle"
-                        href="#"
-                        id="dropdownId"
-                        data-bs-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false"
-                        >My account</a
-                    >
-                    <div
-                        class="dropdown-menu"
-                        aria-labelledby="dropdownId"
-                    >
-                        <a class="dropdown-item" href="#"
-                            >Action 1</a
-                        >
-                        <a class="dropdown-item" href="#"
-                            >Action 2</a
-                        >
-                    </div>
-                
-        </div>
-    </div>
-   </nav>
-
+@section('content')
     <div class=" p-5 mb-4 bg-light rounded-3">
         <div class="container-fluid py-5">
             <h1 class="display-5 fw-bold">Welcome to my blog</h1>
@@ -99,73 +29,22 @@
             </button>
                
         </div>
+
         <hr>
-        <div class="post">
-            <h3>Post title</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, accusantium. Eum dolore pariatur nobis cupiditate odio, laudantium unde esse excepturi fugit. Debitis aliquid, totam molestiae neque iusto placeat alias expedita?</p>
-            <button
-                type="button"
-                class="btn btn-primary"
-            >
-                Read more
-            </button>
-               
+
+            <div class="post">
+                <h3>Post title</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, accusantium. Eum dolore pariatur nobis cupiditate odio, laudantium unde esse excepturi fugit. Debitis aliquid, totam molestiae neque iusto placeat alias expedita?</p>
+                <button
+                    type="button"
+                    class="btn btn-primary"
+                >
+                    Read more
+                </button>
+                
+            </div>
         </div>
-        <hr>
-        <div class="post">
-            <h3>Post title</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, accusantium. Eum dolore pariatur nobis cupiditate odio, laudantium unde esse excepturi fugit. Debitis aliquid, totam molestiae neque iusto placeat alias expedita?</p>
-            <button
-                type="button"
-                class="btn btn-primary"
-            >
-                Read more
-            </button>
-               
-        </div>
-        <hr>
-        <div class="post">
-            <h3>Post title</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, accusantium. Eum dolore pariatur nobis cupiditate odio, laudantium unde esse excepturi fugit. Debitis aliquid, totam molestiae neque iusto placeat alias expedita?</p>
-            <button
-                type="button"
-                class="btn btn-primary"
-            >
-                Read more
-            </button>
-               
-        </div>
-        <hr>
-        </div>
-        <div
-            class="col-md-3 "
-        >
-        <h3>Sidebar</h3>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, tenetur doloremque. Explicabo architecto ut veritatis corrupti veniam perferendis blanditiis, quo, fugit nam velit odit adipisci tenetur quibusdam omnis! Ex, voluptate?
-        </div>
-        
     </div>
-   </div>
    
-   
-        
-    </div>
+@endsection
     
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  </body>
-</html>

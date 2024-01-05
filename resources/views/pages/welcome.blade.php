121a8aa89b50bce9bcef77ca36614757a5035107
Kristofers Jēkabs Rozevskis <44755996+Rozevskis@users.noreply.github.com>
2024-01-05 11:16:35 +0200

    Added bootstrap

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,44 +1,13 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About {{$data['fullName']}}</title>
-    <style>
-        html,body{
-            height: 100%;
-        }
-
-        body{
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            display: table;
-            font-weight: 100;
-
-        }
-        .container{
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
-        .content{
-            text-align: center;
-            display: inline-block;
-        }
-        .title{
-            font-size: 96px;
-        }
-    </style>
-</head>
-<body>
-    <div class="container">
-        <div class="content">
-            <div class="title">About {{$data['fullName']}}</div>
-            <p>Email me at {{$data['email']}}</p>
-            
-        </div>
-    </div>
-    
-</body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel Blog</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
 </html>
